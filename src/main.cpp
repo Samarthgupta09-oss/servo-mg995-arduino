2e31588f39d04020404d84d813c0a1c7df0a328b
Samarthgupta09-oss <samarth.gupta_cs.aiml24@gla.ac.in>
2026-02-04 14:02:46 +0530

    initialize pin

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,28 @@
 #include <Servo.h>
 Servo myServo;
 void setup() {
+    // write your initialization code here
     Serial.begin(9600);
     myServo.attach(10);
     Serial.println("Servo initiate");
 }
 
 void loop() {
-    myServo.write(0);
-    delay(500);
-    myServo.write(90);
-    delay(500);
-    myServo.write(120);
-    delay(500);
-    myServo.write(180);
-    delay(500);
+    // write your code here
+    for (int angle = 0; angle < 180; angle += 10)
+    {
+        myServo.write(angle);
+        Serial.print("Angle");
+        Serial.println(angle);
+        delay(500);
+    }
+    for (int angle = 180; angle >= 0; angle -= 10)
+    {
+        myServo.write(angle);
+        Serial.print("Angle");
+        Serial.println(angle);
+        delay(500);
+    }
+
 
 }
